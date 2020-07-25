11ec035f25df289910691edb047a65b2b7f8233a
sainitin43 <61263651+sainitin43@users.noreply.github.com>
2020-07-26 00:56:22 +0530

    Clean code
    clean

--- sici.java
+++ sici.java
@@ -11,7 +11,6 @@ class sici
     System. out. println("Enter the No.of years:");
     t=sc.nextDouble();
     System. out. println("Enter the Rate of  interest");
-    rate=sc.nextDouble();
     sim=(pr * t * rate)/100;
     com=pr * Math.pow(1.0+rate/100.0,t) - pr;
     System.out.println("Simple Interest="+sim);
